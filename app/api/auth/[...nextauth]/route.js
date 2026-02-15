b218c1341d1a6a40244bddb5532da21d90bb78e2
Prabhakar06021992 <svvprabhakar77@gmail.com>
2026-02-16 01:27:04 +0530

    Fixed nextauth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,9 @@
-export {GET ,POST} from '@/api/auth';
+//export {GET ,POST} from '@/api/auth';
+
+import NextAuth from "next-auth";
+
+const handler = NextAuth({
+  providers: []
+});
+
+export { handler as GET, handler as POST };
